928bbbccaa672433211f7ac56899832758c410c9
AntonioMoralesPerez <antmorper8@gmail.com>
2025-05-02 10:29:26 +0200

    prepare example on plane group P3 (#13)

--- examples/pg13.jl
+++ examples/pg13.jl
@@ -1,26 +1,31 @@
+# This example shows a particular plane group (P3 #13) which doesn't have inversion and 
+# do contain a complex representation. Good example for testing TRS.
+
 using Pkg
 Pkg.activate(@__DIR__)
-
 using Crystalline, TETB
 
-sgnum = 13
-brs = calc_bandreps(sgnum, Val(2))
-coefs = [1, 1, 0, 0, 0, 1]
+pgnum, D = 13, 2
+timereversal = false
+brs = calc_bandreps(pgnum, Val(D); timereversal)
+coefs = zeros(length(brs))
+
+# we want to pick EBRs (1c|A) and (1b|A), which position will depend on timereversal
+if timereversal
+    br₁ = brs[1]
+    br₂ = brs[3]
+    coefs[[1, 3]] .= 1
+else
+    br₁ = brs[1]
+    br₂ = brs[4]
+    coefs[[1, 4]] .= 1
+end
+
 cbr = CompositeBandRep(coefs, brs)
 
-## debug for the BUG #1
-
-br1 = brs[1]
-br2 = brs[2]
-br3 = brs[6]
-
-## because the problem can be seen in block [1,1], I will study here the term involving 
-## br1 → br1.
-
+# chose a representative jump until where we will consider hopping terms.
 Rs = [[0, 0]]
 
-δss = TETB.obtain_symmetry_related_hoppings(Rs, br1, br1)
-
-println(δss[RVec([0, 0])][1])
+hops = obtain_symmetry_related_hoppings(Rs, br₁, br₂)
 
 # this results in a vector v bigger than expected probably the sort of error.
