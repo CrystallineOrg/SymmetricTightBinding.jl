d5027ed3497e8391d248a7bbad028a334e8fbe29
AntonioMoralesPerez <antmorper8@gmail.com>
2025-05-21 10:26:50 +0200

    move the code related to the tight-binding model into submodule `SymmetryTightBinding.jl`

--- src/TETB.jl
+++ src/TETB.jl
@@ -1,22 +1,17 @@
 module TETB
 
-# -------------------- Necessary modules for the package ---------------------------------- #
-using LinearAlgebra
+# -------------------- Necessary modules for the package --------------------------------- #
 using Crystalline
 using MPBUtils
+# TODO: ↓ probably not necessary to add (`SymmetryBases` and `PhotonicBandConnectivity`) 
+#         after your changes in `MPBUtils.jl`
 using SymmetryBases
 using PhotonicBandConnectivity
+#
 using Crystalline: AbstractSymmetryVector, irdim, CompositeBandRep_from_indices, translation
 using Crystalline: reduce_translation_to_unitrange, constant, free, isapproxin, orbit
-using BlockArrays
-using RowEchelon: rref, rref!           # for `poormans_sparsification`
-# -------------------- Predefined constant used ------------------------------ #
-const NULLSPACE_ATOL_DEFAULT = 1e-5
-const SPARSIFICATION_ATOL_DEFAULT = 1e-10
-const PRUNE_ATOL_DEFAULT = SPARSIFICATION_ATOL_DEFAULT
-const ZASSENHAUS_ATOL_DEFAULT = NULLSPACE_ATOL_DEFAULT
-# ---------------------------------------------------------------------------------------- #
 
+# -------------------- Import MPB and Meep ----------------------------------------------- #
 using PyCall
 
 const mp = PyNULL()
@@ -34,39 +29,18 @@ end
 
 export mp, mpb
 
-# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------------- #
 
-include("types.jl")
-export TightBindingCandidateSet
-export HoppingOrbit
-export TightBindingElementString
-export TightBindingBlock
-export TightBindingModel
-export ParameterizedTightBindingModel
+include("SymmetricTightBinding.jl/SymmetricTightBinding.jl")
+
 include("conversion.jl")
-include("show.jl")
+
 include("constrained_nonnegative_expansions.jl")
 include("utils.jl")
 export find_apolar_modes
 export find_auxiliary_modes
 export find_bandrep_decompositions
 export obtain_symmetry_vectors
-include("site_representations.jl")
-export sgrep_induced_by_siteir
-include("tightbinding.jl")
-export obtain_symmetry_related_hoppings
-export tb_hamiltonian
-include("zassenhaus.jl")
-include("timereversal.jl")
-include("hermiticity.jl")
-include("show_tightbinding.jl")
-include("utils_tightbinding.jl")
-include("symmetry_analysis_tightbinding.jl")
-export symmetry_analysis
-include("spectrum_tightbinding.jl")
-export spectrum
-include("plotting_tightbinding.jl") # convert to package extension for Makie
-export plot
 
-# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------------- #
 end # module
