a49f5934a99444dde31358df506f89314711b2d3
AntonioMoralesPerez <antmorper8@gmail.com>
2025-05-21 16:41:20 +0200

    separate `SymmetricTightBinding` as an independent package and make it a dependency of `TETB`

--- src/TETB.jl
+++ src/TETB.jl
@@ -1,20 +1,13 @@
 module TETB
 
-# -------------------- Necessary modules for the package ---------------------------------- #
-using LinearAlgebra
+# -------------------- Necessary modules for the package --------------------------------- #
 using Crystalline
 using MPBUtils
+# TODO: ↓ probably unnecessary dependency
 using PhotonicBandConnectivity
 using Crystalline: AbstractSymmetryVector, irdim, CompositeBandRep_from_indices, translation
 using Crystalline: reduce_translation_to_unitrange, constant, free, isapproxin, orbit
-using BlockArrays
-using RowEchelon: rref, rref!           # for `poormans_sparsification`
 using PythonCall: pynew, pycopy!, pyimport, Py, pyconvert
-# -------------------- Predefined constant used ------------------------------ #
-const NULLSPACE_ATOL_DEFAULT = 1e-5
-const SPARSIFICATION_ATOL_DEFAULT = 1e-10
-const PRUNE_ATOL_DEFAULT = SPARSIFICATION_ATOL_DEFAULT
-const ZASSENHAUS_ATOL_DEFAULT = NULLSPACE_ATOL_DEFAULT
 # ---------------------------------------------------------------------------------------- #
 
 using PythonCall
@@ -34,35 +27,13 @@ export mp, mpb
 
 # ---------------------------------------------------------------------------- #
 
-include("types.jl")
-export TightBindingCandidateSet
-export HoppingOrbit
-export TightBindingElementString
-export TightBindingBlock
-export TightBindingModel
-export ParameterizedTightBindingModel
 include("conversion.jl")
-include("show.jl")
 include("constrained_nonnegative_expansions.jl")
 include("utils.jl")
 export find_apolar_modes
 export find_auxiliary_modes
 export find_bandrep_decompositions
 export obtain_symmetry_vectors
-include("site_representations.jl")
-export sgrep_induced_by_siteir
-include("tightbinding.jl")
-export obtain_symmetry_related_hoppings
-export tb_hamiltonian
-include("zassenhaus.jl")
-include("timereversal.jl")
-include("hermiticity.jl")
-include("show_tightbinding.jl")
-include("utils_tightbinding.jl")
-include("symmetry_analysis_tightbinding.jl")
-export symeigs_analysis
-include("spectrum_tightbinding.jl")
-export spectrum
 
 # ---------------------------------------------------------------------------- #
 end # module
