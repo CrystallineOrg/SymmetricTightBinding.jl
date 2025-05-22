7be7a1afd24ed764f3f1fa7cd761d15910757352
Thomas Christensen <tchr@mit.edu>
2025-05-22 11:48:42 +0200

    transfer a remaining example

--- SymmetricTightBinding/examples/graphene.jl
+++ SymmetricTightBinding/examples/graphene.jl
@@ -1,9 +1,9 @@
 using Pkg
 Pkg.activate(@__DIR__)
-using Crystalline, TETB
+using Crystalline, SymmetricTightBinding
 
 sgnum, D = 17, 2
 brs = calc_bandreps(sgnum, Val(D))
 cbr = @composite brs[5]
 
-tb_model = TETB.tb_hamiltonian(cbr)
+tb_model = tb_hamiltonian(cbr)
